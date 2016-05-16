99eaa02757d8d7a53638c04167f4ea8e1d9281f7
D1abolos <evilsource56@gmail.com>
2016-05-16 12:36:46 +0500

    laba 3 web

---
+++ l3.ru/www/php/send.php
@@ -0,0 +1,36 @@
+<?php 
+header("Content-Type: text/html; charset=utf-8"); 
+session_start();
+if ($_SESSION['send'] == $_SERVER['REMOTE_ADDR'])  
+{
+$login = $password = $email = '';
+$_SESSION['message'] = ''; 
+$flag = true;
+if (!empty($_GET["login"])) 
+	$login = $_GET["login"];
+else {$flag = false ; $_SESSION['message'] = 'Неверно указан login!'; }
+if (!empty($_GET["password"])) 
+	$password = $_GET["password"];
+else {$flag = false ; $_SESSION['message'] = 'Неверно указан password!'; }
+if (filter_var($_GET["email"], FILTER_VALIDATE_EMAIL) && !empty($_GET["email"])) 
+    $email = $_GET["email"]; 
+else {$flag = false ; $_SESSION['message'] = 'Неверно указан email!'; }
+if($flag == true)
+{
+$_SESSION['message'] = 'Done'; 
+$f = fopen('file.txt', 'a'); 
+fwrite($f, $login." \n");
+fwrite($f, $password." \n"); 
+fwrite($f, $email." \r\n"); 
+fclose($f); 
+}
+}
+else {$_SESSION['message'] = 'Error';}
+$back = $_SERVER['HTTP_REFERER']; 
+echo "
+<html>
+  <head>
+   <meta http-equiv='Refresh' content='0; URL=".$_SERVER['HTTP_REFERER']."'>
+  </head>
+</html>";
+?>
